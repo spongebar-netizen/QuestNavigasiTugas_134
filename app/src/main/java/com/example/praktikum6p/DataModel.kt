b2bffe33996906a727e9f938808e0996ebe374c7
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:44:01 +0700

    menambahkan class formulir

--- app/src/main/java/com/example/praktikum6p/DataModel.kt
+++ app/src/main/java/com/example/praktikum6p/DataModel.kt
@@ -1,2 +1,8 @@
 package com.example.praktikum6p
 
+data class DataFormulir(
+    var nama: String = "",
+    var alamat: String = "",
+    var jenisKelamin: String = "",
+    var status: String = ""
+)
