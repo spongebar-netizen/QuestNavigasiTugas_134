b476ad6f3366f2368e8d940d31461f11f08b3252
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:37:19 +0700

    membuat function list peserta

--- app/src/main/java/com/example/praktikum6p/view/ListPesertaScreen.kt
+++ app/src/main/java/com/example/praktikum6p/view/ListPesertaScreen.kt
@@ -1,2 +1,35 @@
 package com.example.praktikum6p.view
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.praktikum6p.DataViewModel
+import com.example.praktikum6p.Navigasi
+import com.example.praktikum6p.R
 
+@Composable
+fun ListPesertaScreen(
+    navController: NavController,
+    viewModel: DataViewModel = viewModel(),
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(LightBackground)
+            .systemBarsPadding()
+    ) {
