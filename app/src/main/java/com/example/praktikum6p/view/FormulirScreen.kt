0ff7b559bef65c39bd6201bbf13a8f15cdb200f5
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:32:49 +0700

    membuat fucntion formulir

--- app/src/main/java/com/example/praktikum6p/view/FormulirScreen.kt
+++ app/src/main/java/com/example/praktikum6p/view/FormulirScreen.kt
@@ -24,3 +24,33 @@ import com.example.praktikum6p.R
 val HeaderPurple = Color(0xFF9C27B0)
 val ButtonPurple = Color(0xFF7B1FA2)
 val LightBackground = Color(0xFFF3E5F5)
+
+@Composable
+fun FormulirScreen(
+    navController: NavController,
+    viewModel: DataViewModel = viewModel(),
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    // --- STATE BARU UNTUK KONTROL POP-UP ---
+    var showDialog by remember { mutableStateOf(false) }
+    // ------------------------------------
+
+    val jenisKelaminOptions = listOf(
+        stringResource(R.string.option_pria),
+        stringResource(R.string.option_wanita)
+    )
+    val statusOptions = listOf(
+        stringResource(R.string.option_janda),
+        stringResource(R.string.option_lajang),
+        stringResource(R.string.option_duda)
+    )
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(LightBackground)
+            .systemBarsPadding()
+            .verticalScroll(rememberScrollState())
+    ) {
