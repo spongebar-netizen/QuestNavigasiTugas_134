dcbfbdc576e804d800685f6b778eb3cd09849d37
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:47:15 +0700

    melengkapi bagian navigasi

--- app/src/main/java/com/example/praktikum6p/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6p/Navigasi.kt
@@ -9,3 +9,31 @@ import androidx.navigation.compose.rememberNavController
 import com.example.praktikum6.view.FormulirScreen
 import com.example.praktikum6.view.ListPesertaScreen
 import com.example.praktikum6.view.WelcomeScreen
+
+enum class Navigasi {
+    Welcome,
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    val viewModel: DataViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = Navigasi.Welcome.name
+    ) {
+        composable(route = Navigasi.Welcome.name) {
+            WelcomeScreen(navController = navController)
+        }
+        composable(route = Navigasi.Formulirku.name) {
+            FormulirScreen(navController = navController, viewModel = viewModel)
+        }
+        composable(route = Navigasi.Detail.name) {
+            ListPesertaScreen(navController = navController, viewModel = viewModel)
+        }
+    }
+}
