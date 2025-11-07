d7102aca150a075b1c457744ac48b0f9b5dc5ffe
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:44:58 +0700

    menambahkan import di data view

--- app/src/main/java/com/example/praktikum6p/DataViewModel.kt
+++ app/src/main/java/com/example/praktikum6p/DataViewModel.kt
@@ -1,4 +1,6 @@
 package com.example.praktikum6p
-
-class DataViewModel {
-}
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
