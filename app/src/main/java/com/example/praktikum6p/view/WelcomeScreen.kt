fca3e572cfed2385ca7ebcd0f87fb8e57e0ed545
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:41:09 +0700

    menambahkan warna

---
+++ app/src/main/java/com/example/praktikum6p/view/WelcomeScreen.kt
@@ -0,0 +1,28 @@
+package com.example.praktikum6p.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.praktikum6p.Navigasi
+import com.example.praktikum6p.R
+
+val DarkPurple = Color(0xFFCA00EE)
+val LightPurple = Color(0xF8EE93FC)
+val CustomColor1 = Color(0xFFD32F2F)
+val CustomColor2 = Color(0xFF1976D2)
