762e670988bcca3e97cc83b73d51f9d261d7d5c0
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:45:27 +0700

    menambahkan function nama alamat jenis kelamin dan status

--- app/src/main/java/com/example/praktikum6p/DataViewModel.kt
+++ app/src/main/java/com/example/praktikum6p/DataViewModel.kt
@@ -4,3 +4,21 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+
+class DataViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(DataFormulir())
+    val uiState: StateFlow<DataFormulir> = _uiState.asStateFlow()
+
+    fun setNama(nama: String) {
+        _uiState.update { currentState -> currentState.copy(nama = nama) }
+    }
+    fun setAlamat(alamat: String) {
+        _uiState.update { currentState -> currentState.copy(alamat = alamat) }
+    }
+    fun setJenisKelamin(jenisKelamin: String) {
+        _uiState.update { currentState -> currentState.copy(jenisKelamin = jenisKelamin) }
+    }
+    fun setStatus(status: String) {
+        _uiState.update { currentState -> currentState.copy(status = status) }
+    }
+}
