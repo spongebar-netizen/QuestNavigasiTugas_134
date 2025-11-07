0f8013ea106b5eb750c59a701cc4012642688606
Lalu Fatan <lalufatan25@gmail.com>
2025-11-07 18:46:29 +0700

    menambahkan import bagian navigasi

---
+++ app/src/main/java/com/example/praktikum6p/Navigasi.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum6p
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum6.view.FormulirScreen
+import com.example.praktikum6.view.ListPesertaScreen
+import com.example.praktikum6.view.WelcomeScreen
